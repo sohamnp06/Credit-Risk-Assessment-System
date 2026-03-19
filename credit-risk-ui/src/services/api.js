be596de5d330334991eaecbe100ff1d04d79aec2
sohamnp06 <sohampashilkar84@gmail.com>
2026-03-19 22:02:15 +0530

    Assessment Form and model integration

---
+++ credit-risk-ui/src/services/api.js
@@ -0,0 +1,13 @@
+const BASE_URL = "http://127.0.0.1:5000";
+
+export const predictRisk = async (data) => {
+  const response = await fetch(`${BASE_URL}/predict`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  return response.json();
+};
